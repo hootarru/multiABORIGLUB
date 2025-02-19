d5abc9c261e5734bc80c8c2b71488f3a382ad2dc
anna <marininavv1177@icloud.com>
2025-02-19 11:05:59 -0800

    example

--- javascripts/javascripts.js
+++ javascripts/javascripts.js
@@ -1,1 +1,14 @@
-console.log("ervjoelirjgoeir");
+console.log("privet");
+
+// const square = document.getElementById("square");
+// square.addEventListener("click", () => {
+//   square.style.transition = "all 3s";
+//   square.style.width = "100px"; // начальная ширина
+//   square.style.height = "100px"; // начальная высота
+//   square.style.borderRadius = "50%"; // радиус круга
+// });
+
+function changeColor(newColor) {
+  const elem = document.getElementById("example");
+  elem.style.color = newColor;
+}
