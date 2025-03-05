6e6100e2dc30ae2b91272f73608872ac02ec29ef
anna <marininavv1177@icloud.com>
2025-03-05 15:48:52 -0800

    primer

--- javascripts/javascripts.js
+++ javascripts/javascripts.js
@@ -1,14 +1,1 @@
 console.log("privet");
-
-// const square = document.getElementById("square");
-// square.addEventListener("click", () => {
-//   square.style.transition = "all 3s";
-//   square.style.width = "100px"; // начальная ширина
-//   square.style.height = "100px"; // начальная высота
-//   square.style.borderRadius = "50%"; // радиус круга
-// });
-
-function changeColor(newColor) {
-  const elem = document.getElementById("example");
-  elem.style.color = newColor;
-}
